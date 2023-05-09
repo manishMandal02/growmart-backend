a5a3b47bf73dc99652a705c0b869d4a1c05029d4
Manish Mandal <manishmandalj@gmail.com>
2023-05-09 14:33:17 +0530

    fix: paypal client

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 
-app.get('/api/config/paypal', (req, res) => res.send('Under Development'));
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
 
 app.use(notFoundError);
 app.use(errorHandler);
