e48128907e66db2e2bab53b98687ef63027755ec
Manish Mandal <manishmandalj@gmail.com>
2023-05-09 13:17:20 +0530

    updated env variables to deploy the project on render

--- Config/Cloudinary.js
+++ Config/Cloudinary.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = { cloudinary };
